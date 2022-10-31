f551df0d204be528ad0262bda8c9acbceaaa5d8a
Keegan Merck <72936078+KeeganMerck@users.noreply.github.com>
2022-10-31 19:14:11 -0400

    Adding basic login look and post

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from urllib import request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+users={}
+
 @app.get('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.post('/')
+def loginPost():
+    print("hello   ;ajskdlfjasdfj")
+    email = request.form.get('loginEmail')
+    password = request.form.get('loginPassword')
+    if email == "" or password =="":
+        print('hi')
+        return redirect('/')
+    print(password)
+    users.update({email: password})
+    return redirect('/')
