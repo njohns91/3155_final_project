faac22704dc634a53d791f1331a6b61000732a18
SethSpoon <sethwspoon@outlook.com>
2022-11-01 10:54:46 -0400

    Added flask apps and submit button

--- app.py
+++ app.py
@@ -15,3 +15,20 @@ def loginPost():
     password = request.form.get('loginPassword')
     users.update({email: password})
     return redirect('/') ##needs to be updated to marketplace page when that implementation is added
+
+
+@app.post('/')
+def create_item():
+    item_name = request.form.get('product_title')
+    item_description = request.form.get('product_description')
+    item_cetegory = request.form.get('product_category')
+    item_price = request.form.get('product_price')
+    return redirect('/') ##needs to be updated to profile page when that implementation is added
+
+@app.post('/')
+def update_item():
+    item_name = request.form.get('product_title')
+    item_description = request.form.get('product_description')
+    item_cetegory = request.form.get('product_category')
+    item_price = request.form.get('product_price')
+    return redirect('/') ##needs to be updated to profile page when that implementation is added
