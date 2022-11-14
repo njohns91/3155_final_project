985a6d726200badf1e6473446b24c2e9779e5cf4
Amine Aboualkacim <aaboualk@uncc.edu>
2022-11-13 22:48:04 -0500

    Implemented the get functions for app.py, implemented the connection between the profile page and the create_listing, update_listing pages

--- app.py
+++ app.py
@@ -7,11 +7,31 @@ users={}
 
 @app.get('/')
 def home():
-    return render_template('login.html')
+    return render_template('login.html') ## Currently set to listing/market page 
+
+@app.get('/profile')
+def profile():
+    return render_template('profile.html')
 
 @app.post('/')
 def loginPost():
     email = request.form.get("loginEmail")
     password = request.form.get('loginPassword')
     users.update({email: password})
-    return redirect('/') ##needs to be updated to marketplace page when that implementation is added
+    return redirect('/market_place') ##needs to be updated to marketplace page when that implementation is added
+
+@app.get('/market_place')
+def market():
+    return render_template('market_place.html')
+
+@app.get('/signout')
+def signout():
+    return redirect('/')
+
+@app.get('/create_listing')
+def create():
+    return render_template('create_listing.html')
+
+@app.get('/update_listing')
+def update():
+    return render_template('update_listing.html')
