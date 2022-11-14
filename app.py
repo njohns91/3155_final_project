a4a391f7d2bdec9f64479ed4d053a7c72cf7fe0c
Amine Aboualkacim <aaboualk@uncc.edu>
2022-11-13 22:48:04 -0500

    Implemented the get functions for app.py, implemented the connection between the profile page and the create_listing, update_listing pages

--- app.py
+++ app.py
@@ -15,33 +15,31 @@ users={}
 
 @app.get('/')
 def home():
-    return render_template('login.html')
+    return render_template('login.html') ## Currently set to listing/market page 
+
+@app.get('/profile')
+def profile():
+    return render_template('profile.html')
 
 @app.post('/')
 def loginPost():
     email = request.form.get("loginEmail")
     password = request.form.get('loginPassword')
-    person = Person('Seth', 'Seth', email, "Hello", password, "Testing bio")
-    db.session.add(person)
-    db.session.commit()
-    return redirect('market_place.html') ##needs to be updated to marketplace page when that implementation is added
+    users.update({email: password})
+    return redirect('/market_place') ##needs to be updated to marketplace page when that implementation is added
 
+@app.get('/market_place')
+def market():
+    return render_template('market_place.html')
 
-@app.post('/')
-def create_item():
-    item_name = request.form.get('product_title')
-    item_description = request.form.get('product_description')
-    item_cetegory = request.form.get('product_category')
-    item_price = request.form.get('product_price')
-    listing = Listing(item_description, item_name, item_cetegory, None, item_price)
-    db.session.add(listing)
-    db.session.commit()
-    return redirect('/') ##needs to be updated to profile page when that implementation is added
+@app.get('/signout')
+def signout():
+    return redirect('/')
 
-@app.post('/')
-def update_item():
-    item_name = request.form.get('product_title')
-    item_description = request.form.get('product_description')
-    item_cetegory = request.form.get('product_category')
-    item_price = request.form.get('product_price')
-    return redirect('/') ##needs to be updated to profile page when that implementation is added
+@app.get('/create_listing')
+def create():
+    return render_template('create_listing.html')
+
+@app.get('/update_listing')
+def update():
+    return render_template('update_listing.html')
