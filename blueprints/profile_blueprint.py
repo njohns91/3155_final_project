76cde1ce66ad2e2fa5e63d838eb920175a64fd43
Amine Aboualkacim <aaboualk@uncc.edu>
2022-11-28 21:38:35 -0500

    Updated profile and fixed the images path way

--- blueprints/profile_blueprint.py
+++ blueprints/profile_blueprint.py
@@ -1,6 +1,7 @@
 from flask import render_template, Blueprint, session, redirect
+from src.repositories.user_repository import user_repository_singleton
 from src.repositories.listing_repository import listing_repository_singleton
-from src.models.models import db, Listing
+from src.models.models import db, Listing, Person
 
 router = Blueprint('profile', __name__, template_folder='templates')
 
@@ -10,5 +11,7 @@ router = Blueprint('profile', __name__, template_folder='templates')
 def profile(): #person_id from current user session
     if 'person' not in session:
         return redirect('/') 
-    #profile_listing = listing_repository_singleton.profile_listing(person_id)
-    return render_template('profile.html') #Listing=profile_listing
+    person_id = session['person']['person_id']
+    person_info = user_repository_singleton.person_info(person_id)
+    user_listings = listing_repository_singleton.get_user_listings(person_id)
+    return render_template('profile.html', Person=person_info, user_list=user_listings)
