84a901b4c15ddfe7423663bfec38175f93ed325c
Keegan Merck <72936078+KeeganMerck@users.noreply.github.com>
2022-11-29 19:34:03 -0500

    Fixing update listing

--- blueprints/market_blueprint.py
+++ blueprints/market_blueprint.py
@@ -17,19 +17,10 @@ def market():
     all_listings = listing_repository_singleton.get_all_listing()
     return render_template('market_place.html', market=all_listings)
 
-@router.get('/listing_page')
-def listing():
-    if 'person' not in session:
-        return redirect('/')
-    return render_template('listing_page.html')
-
-
 @router.get('/listing_page/<listing_id>')
 def listing_display(listing_id):
     single_listing = listing_repository_singleton.specific_listing(listing_id)
     return render_template('listing_page.html', Listing=single_listing)
-
-
 
 @router.get('/create_listing')
 def create():
@@ -71,36 +62,44 @@ def create_item():
     return redirect('/market_place')
 
     
-@router.get('/update_listing')
-def update():
+@router.get('/update_listing/<listing_id>')
+def update(listing_id):
     if 'person' not in session:
         return redirect('/')
-    return render_template('update_listing.html')
-
-@router.post('/update_listing/<int:listing_id>')
-def update_item(listing_id):
-
+    
     post_to_update = Listing.query.get(listing_id)
 
+    return render_template('update_listing.html', post_to_update = post_to_update)
 
-    if request.method == 'POST':
-         post_to_update.listing_description = request.form.get('product_description')
-         post_to_update.title = request.form.get('product_title')
-         post_to_update.category = request.form.get('product_category')
-         post_to_update.price = request.form.get('product_price')
-         try:
-            db.session.commit()
-            return redirect('/profile')
-         except:
-            return "There was an issue"
-    else:
-        return render_template('/update_profile', post_to_update = post_to_update)
+@router.post('/update_listing/<listing_id>')
+def update_item(listing_id):
+    if 'person' not in session:
+        return redirect('/')
 
-
-        
-
-
-
+    post_to_update = Listing.query.get(listing_id)
     
+    post_to_update.listing_description = request.form.get('product_description')
+    post_to_update.title = request.form.get('product_title')
+    post_to_update.category = request.form.get('product_category')
+    post_to_update.price = request.form.get('product_price')
 
+    listing_image = request.files['product_image']
     
+    if listing_image.filename == '':
+        return redirect('/create_listing')
+    
+    if listing_image.filename.rsplit('.',1)[1].lower() not in ['jpg', 'jpeg', 'png', 'webp']:
+        return redirect('/create_listing')
+    
+    safe_filename = secure_filename(f'{post_to_update.person_id}-{listing_image.filename}')
+    
+    listing_image.save(os.path.join('static','listing_images', safe_filename))
+
+    post_to_update.listing_image = safe_filename
+    
+    try:
+        db.session.commit()
+        return redirect('/profile')
+    except Exception as e:
+        print(e)
+        return redirect('/profile')
