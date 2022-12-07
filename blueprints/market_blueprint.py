ebbdea90953f35bf7829819ccc8e181a1f5d9a16
Keegan Merck <72936078+KeeganMerck@users.noreply.github.com>
2022-12-07 14:40:06 -0500

    Profile comments search
    Made profile route be /profile/person_id that way you can view others profiles. Ensured only admin access of accounts and listings is provided to correct user. Flipped around comments for better visual. Made sure case insensitive

--- blueprints/market_blueprint.py
+++ blueprints/market_blueprint.py
@@ -9,28 +9,37 @@ from forms import SearchForm
 
 router = Blueprint('market', __name__, template_folder='templates')
 
-
-
 @router.get('/market_place')
 def market():
     if 'person' not in session:
         return redirect('/')
+
+    person_id = session['person']['person_id']
     all_listings = listing_repository_singleton.get_all_listing()
-    return render_template('market_place.html', market=all_listings)
+    return render_template('market_place.html', market=all_listings, person_id=person_id)
 
 @router.get('/listing_page/<listing_id>')
 def listing_display(listing_id):
+    if 'person' not in session:
+        return redirect('/')
+
+    person_id = session['person']['person_id']
     single_listing = listing_repository_singleton.specific_listing(listing_id)
-    return render_template('listing_page.html', Listing=single_listing)
+    return render_template('listing_page.html', Listing=single_listing, person_id=person_id)
 
 @router.get('/create_listing')
 def create():
     if 'person' not in session:
         return redirect('/')
-    return render_template('create_listing.html')
+
+    person_id = session['person']['person_id']
+    return render_template('create_listing.html', person_id=person_id)
 
 @router.post('/create_listing')
 def create_item():
+    if 'person' not in session:
+        return redirect('/')
+
     item_name = request.form.get('product_title')
     item_description = request.form.get('product_description')
     item_cetegory = request.form.get('product_category')
@@ -63,97 +72,126 @@ def create_item():
     flash(f'Listing "{item_name}" was created', 'success')
     return redirect('/market_place')
 
-
-
 @router.post('/create-comment/<listing_id>')
 def create_comment(listing_id):
+    if 'person' not in session:
+        return redirect('/')
+    
     text = request.form.get('text')
     person_id = session['person']['person_id']
-    
 
     if not text:
         flash('Comment cannot be empty.', category='error')
     else:
-        listing = Listing.query.get(listing_id)
         comment = Comment(person_id, listing_id, datetime.now(), text)
         db.session.add(comment)
         db.session.commit()
         
-    return redirect('/market_place')
-
-
+    return redirect(f'/listing_page/{listing_id}')
 
 @router.get('/update_listing/<listing_id>')
 def update(listing_id):
+    #Ensure user is logged in
     if 'person' not in session:
         return redirect('/')
     
     post_to_update = Listing.query.get(listing_id)
+    user_person_id = session['person']['person_id']
+    profile_of_listing = Person.query.get(post_to_update.person_id)
 
-    return render_template('update_listing.html', post_to_update = post_to_update)
+    #Ensure user is tyring to edit own listing
+    isOwner = profile_of_listing.person_id == user_person_id
+    print(isOwner, profile_of_listing.person_id, user_person_id)
+    if not isOwner:
+        flash("Unathorized access", "error")
+        return redirect(f'/profile/{user_person_id}')
+
+    return render_template('update_listing.html', post_to_update = post_to_update, person_id=user_person_id)
 
 @router.post('/update_listing/<listing_id>')
 def update_item(listing_id):
+    #Ensure user is logged in
     if 'person' not in session:
         return redirect('/')
-
+    
     post_to_update = Listing.query.get(listing_id)
+    user_person_id = session['person']['person_id']
+    profile_of_listing = Person.query.get(post_to_update.person_id)
+
+    #Ensure user is tyring to edit own listing
+    isOwner = profile_of_listing.person_id == user_person_id
+    print(isOwner, profile_of_listing.person_id, user_person_id)
+    if not isOwner:
+        flash("Unathorized access", "error")
+        return redirect(f'/profile/{user_person_id}')
     
     post_to_update.listing_description = request.form.get('product_description')
     post_to_update.title = request.form.get('product_title')
     post_to_update.category = request.form.get('product_category')
     post_to_update.price = request.form.get('product_price')
 
-    listing_image = request.files['product_image']
-    
+    listing_image = request.files['product_image']    
     if listing_image.filename == '':
         flash('Must include file for image', 'error')
         return redirect(f'/update_listing/{listing_id}')
     if listing_image.filename.rsplit('.',1)[1].lower() not in ['jpg', 'jpeg', 'png', 'webp']:
         flash('File must be jpg, jpeg, png, or webp', 'error')
-        return redirect(f'/update_listing/{listing_id}')
-    
+        return redirect(f'/update_listing/{listing_id}')    
     safe_filename = secure_filename(f'{post_to_update.person_id}-{listing_image.filename}')
     listing_image.save(os.path.join('static','listing_images', safe_filename))
-
     post_to_update.listing_image = safe_filename
     
     try:
         db.session.commit()
         flash(f'Listing "{post_to_update.title}" was updated', 'success')
-        return redirect('/profile')
+        return redirect(f'/profile/{user_person_id}')
     except Exception as e:
         flash(f'{e}', 'error')
         return redirect(f'/update_listing/{listing_id}')
+
+@router.get('/delete_listing/<listing_id>')
+def delete(listing_id):
+    #Ensure user is logged in
+    if 'person' not in session:
+        return redirect('/')
+    
+    post_to_delete = Listing.query.get(listing_id)
+    user_person_id = session['person']['person_id']
+    profile_of_listing = Person.query.get(post_to_delete.person_id)
+
+    #Ensure user is tyring to edit own listing
+    isOwner = profile_of_listing.person_id == user_person_id
+    print(isOwner, profile_of_listing.person_id, user_person_id)
+    if not isOwner:
+        flash("Unathorized access", "error")
+        return redirect(f'/profile/{user_person_id}')
+    
+    post_to_delete.listing_description = request.form.get('product_description')
+    post_to_delete.title = request.form.get('product_title')
+    post_to_delete.category = request.form.get('product_category')
+    post_to_delete.price = request.form.get('product_price')
+
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        flash('Listing deleted successfully!', 'success')
+        return redirect(f'/profile/{user_person_id}')
+    except Exception as e:
+        flash(f'{e}', 'error')
+        return redirect(f'/profile/{user_person_id}')
 
 @router.post('/search')
 def search():
     if 'person' not in session:
         return redirect('/') 
+
     form = SearchForm()
     listings = Listing.query
+    person_id = session['person']['person_id']
+
     if form.validate_on_submit():
         listing_searched = form.searched.data
-        listings = listings.filter(Listing.title.like('%' + listing_searched + '%'))
+        listings = listings.filter(Listing.title.ilike('%' + listing_searched + '%'))
         listings = listings.order_by(Listing.title).all()
-        return render_template('search.html', form=form, searched = listing_searched, listings=listings)
-    
-@router.get('/delete_listing/<listing_id>')
-def delete(listing_id):
-    if 'person' not in session:
-        return redirect('/')
-    listing_to_delete = Listing.query.get(listing_id)
-    
-    listing_to_delete.listing_description = request.form.get('product_description')
-    listing_to_delete.title = request.form.get('product_title')
-    listing_to_delete.category = request.form.get('product_category')
-    listing_to_delete.price = request.form.get('product_price')
 
-    try:
-        db.session.delete(listing_to_delete)
-        db.session.commit()
-        flash('Listing deleted successfully!', 'success')
-        return redirect('/profile')
-    except Exception as e:
-        flash(f'{e}', 'error')
-        return redirect('/profile')
+        return render_template('search.html', form=form, searched = listing_searched, listings=listings, person_id=person_id)
--- blueprints/profile_blueprint.py
+++ blueprints/profile_blueprint.py
@@ -8,30 +8,52 @@ from security import bcrypt
 
 router = Blueprint('profile', __name__, template_folder='templates')
 
-@router.get('/profile')
-def profile(): #person_id from current user session
-    if 'person' not in session:
-        return redirect('/') 
-    person_id = session['person']['person_id']
-    person_info = user_repository_singleton.person_info(person_id)
-    user_listings = listing_repository_singleton.get_user_listings(person_id)
-    return render_template('profile.html', Person=person_info, user_list=user_listings)
-
-#Update Profile
-
-@router.get('/update_profile/<person_id>')
-def update_profile(person_id):
+@router.get('/profile/<person_id>')
+def profile(person_id): #person_id from current user session
     if 'person' not in session:
         return redirect('/')
+
+    profile_person_info = Person.query.get(person_id)
+    user_listings = listing_repository_singleton.get_user_listings(person_id)
+    
+    #Determines if user is going to own profile or someone elses
+    user_person_id = session['person']['person_id']
+    isOwner = profile_person_info.person_id == user_person_id
+
+    return render_template('profile.html', Person=profile_person_info, user_list=user_listings, person_id=user_person_id, isOwner = isOwner)
+
+#Update Profile
+@router.get('/update_profile/<person_id>')
+def update_profile(person_id):
+    #Ensure user is logged in
+    if 'person' not in session:
+        return redirect('/')
+
+    user_person_id = session['person']['person_id']
     profile_to_update = Person.query.get(person_id)
-    return render_template('update_profile.html', profile_to_update = profile_to_update)
+
+    #Ensure user is tyring to edit own account
+    isOwner = profile_to_update.person_id == user_person_id
+    if not isOwner:
+        flash("Unathorized access", "error")
+        return redirect(f'/profile/{user_person_id}')
+
+    return render_template('update_profile.html', profile_to_update = profile_to_update, person_id=user_person_id)
 
 @router.post('/update_profile/<person_id>')
 def updates_profile(person_id):
+    #Ensure user is logged in
     if 'person' not in session:
         return redirect('/')
-    
+
+    user_person_id = session['person']['person_id']
     profile_to_update = Person.query.get(person_id)
+
+    #Ensure user is tyring to edit own account
+    isOwner = profile_to_update.person_id == user_person_id
+    if not isOwner:
+        flash("Unathorized access", "error")
+        return redirect(f'/profile/{user_person_id}')
 
     profile_to_update.first_name = request.form.get('updateFirst')
     profile_to_update.last_name = request.form.get('updateLast')
@@ -59,16 +81,25 @@ def updates_profile(person_id):
     try:
         db.session.commit()
         flash("Profile Updated", 'success')
-        return redirect('/profile')
+        return redirect(f'/profile/{user_person_id}')
     except Exception as e:
         flash(f'{e}', 'error')
         return redirect(f'/update_profile/{person_id}')
 
 @router.get('/delete_profile/<person_id>')
 def delete(person_id):
+    #Ensure user is logged in
     if 'person' not in session:
         return redirect('/')
+
+    user_person_id = session['person']['person_id']
     profile_to_delete = Person.query.get(person_id)
+
+    #Ensure user is tyring to edit own account
+    isOwner = profile_to_delete.person_id == user_person_id
+    if not isOwner:
+        flash("Unathorized access", "error")
+        return redirect(f'/profile/{user_person_id}')
 
     user_listings = listing_repository_singleton.get_user_listings(person_id)
     
@@ -85,4 +116,4 @@ def delete(person_id):
         return redirect('/')
     except Exception as e:
         flash(f'{e}', 'error')
-        return redirect('/profile')
+        return redirect(f'/profile/{user_person_id}')
